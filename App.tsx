4d8c7ee2912c00795e25db22f337473a0c3d2e35
Luiz Mauro <luizmetec@hotmail.com>
2020-09-26 20:20:27 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Application from "./src/App";
+import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="auto" backgroundColor="#f0f4ff" />
+      <Application />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+import { Container, Title } from "./styles";
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Title>Faça seu Login</Title>
+    </Container>
+  );
+};
+
+export default SignIn;
