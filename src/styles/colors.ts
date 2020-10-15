7380ba98fc5bc4e92f58533a9af9d84d449b3575
Luiz Mauro <luizmetec@hotmail.com>
2020-10-14 21:35:33 -0300

    create file colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  laranja: "#f9965f",
+  azul: "#3c9ce9",
+  bgDefault: "#f0f4ff",
+  colorIconMenu: "#fbffff",
+  fontColorCard: "#535353",
+  colorFontBGLaranja: "#fbffff",
+  colorFontBGAzul: "#fbffff",
+  bgCards: "#ffffff",
+};
