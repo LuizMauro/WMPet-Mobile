a99b42d945d35be34a5f4d3caad0a4e4b0154f13
Luiz Mauro <luizmetec@hotmail.com>
2020-10-18 12:39:07 -0300

    style menu bottom

--- src/pages/privates/PrivateProfile/index.tsx
+++ src/pages/privates/PrivateProfile/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ScrollView } from "react-native";
 import { FloatingAction } from "react-native-floating-action";
 
 import {
--- src/routes/private.routes.tsx
+++ src/routes/private.routes.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Icon from "react-native-vector-icons/Ionicons";
-import Home from "../pages/privates/home";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
+import Home from "../pages/privates/home";
 import { SearchAnimalsRoutes } from "./searchAnimals";
 import { ProfileRoutes } from "./Profile";
 
+import { darken } from "polished";
 import { colors } from "../styles/colors";
 
 const BottomNavigation = createBottomTabNavigator();
@@ -15,20 +16,31 @@ const PrivateRoutes: React.FC = () => (
   <NavigationContainer independent={true}>
     <BottomNavigation.Navigator
       tabBarOptions={{
-        tabStyle: { backgroundColor: colors.azul },
-        labelStyle: { color: colors.colorIconMenu, fontSize: 14 },
+        tabStyle: { backgroundColor: colors.azul, elevation: 1 },
+        labelStyle: { fontSize: 14 },
         iconStyle: {
-          color: colors.colorIconMenu,
+          color: "red",
           fontSize: 24,
+          backgroundColor: "red",
         },
+        activeTintColor: colors.laranja,
+        inactiveTintColor: darken("0.2", "#fff"),
       }}
     >
-      <BottomNavigation.Screen name="Home" component={Home} />
+      <BottomNavigation.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" size={size} color={color} />
+          ),
+        }}
+      />
       <BottomNavigation.Screen
         name="Animais perdidos"
         options={{
           tabBarIcon: ({ color, size }) => (
-            <Icon name="logo-android" size={size} color={color} />
+            <Icon name="pets" size={size} color={color} />
           ),
         }}
         component={SearchAnimalsRoutes}
@@ -38,7 +50,7 @@ const PrivateRoutes: React.FC = () => (
         name="Perfil"
         options={{
           tabBarIcon: ({ color, size }) => (
-            <Icon name="logo-android" size={size} color={color} />
+            <Icon name="person" size={size} color={color} />
           ),
         }}
         component={ProfileRoutes}
